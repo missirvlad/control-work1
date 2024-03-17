681d02d3818c3476ef614b6fe1329791f010540c
missirvlad <147083170+missirvlad@users.noreply.github.com>
2024-03-17 11:57:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,32 @@ def save_notes(notes):
 # Функция для фильтрации заметок по дате
 def filter_notes_by_date(notes, date):
     return [note for note in notes if note["date"] == date]
+def main():
+    notes = load_notes()
+    print("Добро пожаловать в приложение заметки")
+    while True:
+        command = input("Введите команду (Создать/Прочитать/Редактировать/Удалить/Фильтровать(по дате)/Выход): ")
+        if command == "Создать":
+            title = input("Введите заголовок заметки: ")
+            body = input("Введите тело заметки: ")
+            note = {"id": len(notes) + 1, "title": title, "body": body, "date": str(datetime.datetime.now())}
+            notes.append(note)
+            save_notes(notes)
+            print("Заметка успешно сохранена.")
+        elif command == "Прочитать":
+            print("ID | Заголовок | Тело | Дата и время создания")
+            for note in notes:
+                print(f'{note["id"]} | {note["title"]} | {note["body"]} | {note["date"]}')
+
+        elif command == "Редактировать":
+            note_id = int(input("Введите ID заметки для редактирования: "))
+            for note in notes:
+                if note["id"] == note_id:
+                    note["title"] = input("Введите новый заголовок заметки: ")
+                    note["body"] = input("Введите новое тело заметки: ")
+                    note["date"] = str(datetime.datetime.now())
+                    save_notes(notes)
+                    print("Заметка успешно отредактирована.")
+                    break
+                    
+        
